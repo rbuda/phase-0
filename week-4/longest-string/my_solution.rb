f242af9b0912bc0270ac93e2ff9d47828e384af3
rbuda <ryannebuda@gmail.com>
2016-01-22 19:50:35 -0600

    4.6.4 File Upload

---
+++ week-4/longest-string/my_solution.rb
@@ -0,0 +1,19 @@
+# Longest String
+
+# I worked on this challenge [by myself, with: ].
+
+# longest_string is a method that takes an array of strings as its input
+# and returns the longest string
+#
+# +list_of_words+ is an array of strings
+# longest_string(list_of_words) should return the longest string in +list_of_words+
+#
+# If +list_of_words+ is empty the method should return nil
+
+
+# Your Solution Below
+def longest_string(list_of_words)
+	list_of_words.max_by{|x| x.size}
+end
+
+# list_of_words.max { |x,y| x.size <=> y.size}
