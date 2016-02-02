d950ef7171558f15f432cb162be6ad8041233511
rbuda <ryannebuda@gmail.com>
2016-02-02 12:44:55 -0600

    6.2 No Errors

--- week-6/die-2/my_solution.rb
+++ week-6/die-2/my_solution.rb
@@ -6,14 +6,19 @@
 # Pseudocode
 
 # Input: methods :initialize, :size & :roll.
-# Output: 
+# Output: if empty - ArgumentError, return number of sides, return random string of labels
 # Steps:
+# if labels is empty raise ArgumentError
+# set instance variables for labels & sides
+# keep sides same from last challenge
+# set roll method to return random number of sides from labels array
 
 
 # Initial Solution
 
 class Die
   def initialize(labels)
+    labels = []
   	if labels.empty?
   		raise ArgumentError.new("ArgumentError")
   	end
@@ -49,3 +54,17 @@ class Die
 end
 
 # Reflection
+
+
+# What were the main differences between this die class and the last one you created in terms of implementation? Did you need to change much logic to get this to work?
+# the argument was labels versus sides.  Had to create an instance variable to get sides assigned a value for the sides method to work. Not too much logic. The answer for roll took me the most time.
+
+# What does this exercise teach you about making code that is easily changeable or modifiable? 
+# I need to know how to interchangeably use attr_reader/writer/accessors.
+
+# What new methods did you learn when working on this challenge, if any?
+# Used the .empty? method more than I have before.
+
+# What concepts about classes were you able to solidify in this challenge?
+# Instance variables is a newer concept I'm working to get nailed down yet
+
