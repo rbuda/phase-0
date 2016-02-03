db21d7e31802ab7811804abf9d0c4db1d9f79790
rbuda <ryannebuda@gmail.com>
2016-02-03 08:39:00 -0600

    Refactor GPS 2.3

--- week-6/gps2_3.rb
+++ week-6/gps2_3.rb
@@ -1,5 +1,5 @@
 # Your Names
-# 1) Ryan Nebuda
+# 1) Luis Ybarra
 # 2)
 
 # We spent [#] hours on this challenge.
@@ -7,27 +7,36 @@
 # Bakery Serving Size portion calculator.
 
 def serving_size_calc(item_to_make, num_of_ingredients)
+  # Menu of Baked Goods for DBC Bakery
   library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
+  
+  # Error Check 
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      error_counter += -1
-    end
+  unless library.has_key?(item_to_make)
+    raise ArgumentError.new("#{item_to_make} Not Found")
   end
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
+  # End Error Check
 
-  serving_size = library.values_at(item_to_make)[0]
+  # Calculate Serving Size
+  serving_size = library[item_to_make]
   remaining_ingredients = num_of_ingredients % serving_size
-
-  case remaining_ingredients
-  when 0
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}"
+  baked = num_of_ingredients / serving_size
+  
+  suggested_baked_good = ""
+  
+  return_string = "Calculations complete: Make #{baked} of #{item_to_make}"
+  
+  # Check remaining ingredients
+  if remaining_ingredients == 0
+    return return_string
+  elsif remaining_ingredients >= 5
+    suggested_baked_good = "cake"
+    return return_string + ", you have #{remaining_ingredients} leftover ingredients. Suggested baking items: #{suggested_baked_good}"
   else
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+    suggested_baked_good = "cookie"
+    return return_string + ", you have #{remaining_ingredients} leftover ingredients. Suggested baking items: #{suggested_baked_good}"
+    return remaining
   end
 end
 
@@ -37,6 +46,7 @@ p serving_size_calc("cake", 5)
 p serving_size_calc("cake", 7)
 p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
+p serving_size_calc("pie", 13)
 p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
