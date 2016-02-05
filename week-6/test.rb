2c8a7ac9c985120b64779205e1a4d8a9a31c1280
rbuda <ryannebuda@gmail.com>
2016-02-04 18:08:48 -0600

    Test File Update

--- week-6/test.rb
+++ week-6/test.rb
@@ -1,3 +1,19 @@
-array = [rand(200)]
+# class GuessingGame
+#   def initialize(answer)
+#     @answer = answer
+#   end
 
-puts array
+#   def guess(guess)
+#   	@guess = guess
+#   	if @guess > answer
+#   		:high
+#   	elsif @guess < answer
+#   		:low
+#   	else @guess == answer
+#   		:correct
+#   	end
+#   end
+
+# end
+
+
