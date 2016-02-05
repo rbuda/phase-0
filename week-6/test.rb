b0830bc2e243615c2df8c0edf19e601aaa9573ca
rbuda <ryannebuda@gmail.com>
2016-02-05 11:11:26 -0600

    Test File Update

--- week-6/test.rb
+++ week-6/test.rb
@@ -1,14 +1,21 @@
-class Item
-  def initialize(item_name, quantity)
-    @item_name = item_name
-    @quantity = quantity
-  end
+# class Item
+#   def initialize(item_name, quantity)
+#     @item_name = item_name
+#     @quantity = quantity
+#   end
   
-  def show
-    puts @item_name
-    puts @quantity
-  end
-end
+#   def show
+#     puts @item_name
+#     puts @quantity
+#   end
+# end
 
-Item.new("tv",1).show
-Item.new("fridge",1).show
+# Item.new("tv",1).show
+# Item.new("fridge",1).show
+
+
+array = [1,2,3,4,5,6,7,8,9,10]
+
+test = []
+test = array.each { |x| print x += 2 }
+puts test
