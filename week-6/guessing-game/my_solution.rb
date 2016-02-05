f6e8f8dbd03257ef624627833e2bfb42538d92d5
rbuda <ryannebuda@gmail.com>
2016-02-04 20:10:05 -0600

    6.3 Refactor & Reflection

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -1,26 +1,29 @@
 # Build a simple guessing game
 
-
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 # I spent [#] hours on this challenge.
 
 # Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: 
+# answer
+# guess 
 
+# Output:
+# Symbols for high, low & correct
+# Change :solved? sym when correct to show true
+# 
+
+# Steps:
+# initialize method needs to have a single arg. Set instance var answer = answer
+# guess receives single argument
+# guess returns sym high if high (100?)
+# guess returns sym low if low (0?)
+# guess returns correct if answer (10?)
+# incorrect guesses keep :solved? false
+# once guess is correct.  change :solved? from false to true.
 
 # Initial Solution
-
-# Your GuessingGame class should be initialized with an integer called answer.
-
-# Define an instance method GuessingGame#guess which takes an integer called guess as its input. guess should return the symbol :high if the guess is larger than the answer, :correct if the guess is equal to the answer, and :low if theguess is lower than the answer.
-
-# Define an instance method GuessingGame#solved? which returns true if the most recent guess was correct and false otherwise.
-
-# In this case, we're asking you to use symbols for :low, :high, and :correct. This is partly due to the way strings and symbols are stored in memory. 
-
 
 class GuessingGame
   def initialize(answer)
@@ -29,29 +32,58 @@ class GuessingGame
 
   def guess(guess)
   	@guess = guess
-  	if @guess > answer
-  		:high
-  	elsif @guess < answer
-  		:low
-  	else @guess == answer
-  		:correct
+  	if @guess > @answer
+  		return :high
+  	elsif @guess < @answer
+  		return :low
+  	else @guess == @answer
+  		return :correct
   	end
   end
 
-  def solved(guess)
-  	
-  end
-  
+  def solved?
+  	if @guess == @answer
+      return true
+    else 
+      return false
+    end
+  end 
 end
-
-
-
 
 # Refactored Solution
 
+class GuessingGame
+  def initialize(answer)
+    @answer = answer
+    @last_guess = nil
+  end
 
+  def guess(guess)
+    if guess > @answer
+      @last_guess = :high
+    elsif guess < @answer
+      @last_guess = :low
+    elsif guess == @answer
+      @last_guess = :correct
+    end
+    return @last_guess
+  end
 
-
-
+  def solved?
+    @last_guess == :correct ? (return true) : (return false)
+  end 
+end
 
 # Reflection
+
+# How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object? 
+# Real world objects are going to have a class of behaviors or methods that they do.  They will each have their own specific demographics that are applied only to them (instance vars).
+
+# When should you use instance variables? What do they do for you?
+# Store info into a var only for that instance of the class.  If you had a class Soldiers and @name = Tom.  The particular soldier is named Tom of the class Soldiers.
+
+# Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
+# The if else stmts made it easier to flow through the solution.  Struggled with wanting to have solved? changed in the guess method versus solved?
+
+# Why do you think this code requires you to return symbols? What are the benefits of using symbols?
+# Symbols are good for when the user doesn't want something to be altered.  You can create a new symbol but not change one.  Not having this altered at runtime is an advantage and they allow the code to run quicker.
--- week-6/test.rb
+++ week-6/test.rb
@@ -1,19 +1,14 @@
-# class GuessingGame
-#   def initialize(answer)
-#     @answer = answer
-#   end
+class Item
+  def initialize(item_name, quantity)
+    @item_name = item_name
+    @quantity = quantity
+  end
+  
+  def show
+    puts @item_name
+    puts @quantity
+  end
+end
 
-#   def guess(guess)
-#   	@guess = guess
-#   	if @guess > answer
-#   		:high
-#   	elsif @guess < answer
-#   		:low
-#   	else @guess == answer
-#   		:correct
-#   	end
-#   end
-
-# end
-
-
+Item.new("tv",1).show
+Item.new("fridge",1).show
