90ab6853b1e5621e8067fb7e20157bbf528ce939
rbuda <ryannebuda@gmail.com>
2016-02-13 13:40:37 -0600

    manipulating-objects.js

--- week-7/test.js
+++ week-7/test.js
@@ -5,30 +5,30 @@
 // console.log(totalSeconds + " is " + minutes + " minutes and " + seconds + " seconds");
 
 
-var landscape = function() {
-  var result = "";
-  var flat = function(size) {
-    for (var count = 0; count < size; count++)
-      result += "_";
-  };
-  var mountain = function(size) {
-    result += "/";
-    for (var count = 0; count < size; count++)
-      result += "'";
-    result += "\\";
-  };
+// var landscape = function() {
+//   var result = "";
+//   var flat = function(size) {
+//     for (var count = 0; count < size; count++)
+//       result += "_";
+//   };
+//   var mountain = function(size) {
+//     result += "/";
+//     for (var count = 0; count < size; count++)
+//       result += "'";
+//     result += "\\";
+//   };
 
-  flat(3);
-  mountain(4);
-  flat(6);
-  mountain(1);
-  flat(1);
-  return result;
-};
+//   flat(3);
+//   mountain(4);
+//   flat(6);
+//   mountain(1);
+//   flat(1);
+//   return result;
+// };
 
-console.log(landscape());
-// → ___/''''\______/'\_
+// console.log(landscape());
+// // → ___/''''\______/'\_
 
-function square(x) {
-  return x * x;
-}
+// function square(x) {
+//   return x * x;
+// }
