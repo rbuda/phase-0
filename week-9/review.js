9fee2e40472718ab4da75a68fb4eb6c7befc7b39
rbuda <ryannebuda@gmail.com>
2016-02-28 11:41:45 -0600

    Reflection for review.js

--- week-9/review.js
+++ week-9/review.js
@@ -1,53 +1,91 @@
-// Release 1: Create your user stories
-
-// Create a new list
-// Add an item with a quantity to the list
-// Remove an item from the list
-// Update quantities for items in your list
-// Print the list (Consider how to make it look nice!)
-
-// Release 2: Pseudocode
-
-///////// Array or Object??
-
-// Determine which data structure you should use for your grocery list based on these requirements. Does an array or an object the most sense and why?
-
-// Remember to keep your pseudocode in plain English and write it so that it's easy to translate to your initial solution. Identify the input and output. Your pseudocode should also avoid using language-specific words, include proper indentation, and capitalize keywords like IF, WHILE, etc.)
-
-///// Input
-///// Output
-
-
-
 // Release 3: Initial Solution
 
-// Using your pseudocode, create your initial solution. If you've spent the time to write your pseudocode in enough detail, this should be relatively easy.
+// var food = ("fish eggs milk ham");
+// var groceries = {};
 
-// Remember, your initial solution should not include any special JavaScript methods. Keep the logic basic so you can incorporate fun methods when you refactor!
+// var createList = function(list) {
+// 	var foodArray = food.split(" ");
+// 	for(var i = 0; i < foodArray.length; i++){
+// 		groceries[foodArray[i]] = 1;
+// 	}
+// 	console.log(groceries);
+// }
 
+// createList(food);
 
+// var addItem = function(item) {
+// 	groceries[item] = 1;
+// 	console.log(groceries);
+// }
 
+// addItem("bread");
 
+// var deleteItem = function(item) {
+// 	delete groceries[item]
+// 	console.log(groceries);
+// }
+
+// deleteItem("ham");
+
+// var updateList = function(item, quantity) {
+// 	groceries[item] = quantity;
+// 	console.log(groceries);
+// }
+
+// updateList("yogurt", 2);
+// updateList("fish", 2);
+
+// var prettyList = function(groceries) {
+// 	for (item in groceries) {
+// 		console.log(item + "......" + groceries[item]);
+// 	}
+// }
+
+// prettyList(groceries);
 
 // Release 4: Refactor
 
-// Now go into the Docs to see if there are any methods you can simplify your code with. Also refactor for readability. Keep in mind, although one-line solutions look neat, they aren't necessarily the most efficient or "best" code. 
+var food = ("fish eggs milk ham");
+var groceries = {};
+var createList = function(list) {
+	var foodArray = food.split(" ");
+	for(var i = 0; i < foodArray.length; i++){
+		groceries[foodArray[i]] = 1;
+	}
+}
 
+var addItem = function(item) {
+	groceries[item] = 1;
+}
 
+var deleteItem = function(item) {
+	delete groceries[item]
+}
 
+var updateList = function(item, quantity) {
+	groceries[item] = quantity;
+}
 
-
-
-
-
-
+var prettyList = function(groceries) {
+	console.log("Groceries:")
+	console.log("-----------")
+	for (item in groceries) {
+		console.log(item + ": " + groceries[item]);
+	}
+	console.log("-----------")
+}
+createList(food);
+addItem("bread");
+deleteItem("ham");
+updateList("yogurt", 2);
+updateList("fish", 2);
+prettyList(groceries);
 
 // Release 5: Reflect
 
-// Create a commented reflection section in your "review.js" file. In it answer the following questions:
-
 // What concepts did you solidify in working on this challenge? (reviewing the passing of information, objects, constructors, etc.)
+// I spent a good amount of time starting over with JavaScript this week.  Feel the lightbulb came on and am confident going into Monday.
 
-// What was the most difficult part of this challenge?
+// What was the most difficult part of this challenge? Getting the groceries list object created from a string.  Was going to just start off with the list in an array but thought it was kind of cheating.
 
-// Did an array or object make more sense to use and why?
+// Did an array or object make more sense to use and why? Object.  Bracket notation was easier to add the array into and automatically assign each a value of 1.  Intially had a value of zero but didn't make sense to have something on the lise and not "buy it".
